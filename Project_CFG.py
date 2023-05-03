3a7c884e8be2f45f6fe8ceff5d73062f62f39e4e
Onyeoma A <123620692+Onyeoma-A@users.noreply.github.com>
2023-05-03 19:09:01 +0100

    Add files via upload

--- Project_CFG.py
+++ Project_CFG.py
@@ -15,9 +15,9 @@ user_choice = str(input("Please enter your choice:   "
 if user_choice == "1":
     Savings_Amount = int(input("Please enter the amount you want to deposit:  "))
     Years = int(input("Please enter the amount of years you want to invest for: "))
-    NatWest = Savings_Amount * 0.0525**Years
-    Aviva = Savings_Amount * 0.05 ** Years
-    Llyods = Savings_Amount * 0.04 ** Years
+    NatWest = Savings_Amount * 1.0525**Years - (Savings_Amount)
+    Aviva = Savings_Amount * 1.05 ** Years - (Savings_Amount)
+    Llyods = Savings_Amount * 1.04 ** Years - (Savings_Amount)
 
     print(f"Natwest will be: {NatWest}")
     print(f"Aviva will be: {Aviva}")
