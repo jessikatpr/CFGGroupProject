51246e1d52691baf0784e85343fe43844bbcf2a3
lay <97245251+LaylaSC@users.noreply.github.com>
2023-05-22 22:20:58 +0100

    add tests to test_Investment.py

--- unittest_Investment.py
+++ unittest_Investment.py
@@ -1,5 +1,7 @@
 import unittest
-from Investment import Investment, Bond
+from Investment import Investment, Bond, Stock, Portfolio, MutualFund
+from unittest.mock import patch
+
 
 class TestInvestment(unittest.TestCase):
     def test_investment(self):
@@ -28,6 +30,93 @@ class TestInvestment(unittest.TestCase):
         self.assertEqual(bond.purchase_price, purchase_price)
         self.assertEqual(bond.type, "bond")
         self.assertEqual(bond.coupon_rate, coupon_rate)
+
+    def test_stock(self):
+        symbol = "TSLA"
+        name = "Tesla Inc."
+        quantity = 50
+        purchase_price = 1000.0
+        stock = Stock(symbol, name, quantity, purchase_price)
+
+        self.assertEqual(stock.symbol, symbol)
+        self.assertEqual(stock.name, name)
+        self.assertEqual(stock.quantity, quantity)
+        self.assertEqual(stock.purchase_price, purchase_price)
+        self.assertEqual(stock.type, "stock")
+
+    def test_mutualfund(self):
+        symbol = "TSLA"
+        name = "Tesla Inc."
+        quantity = 50
+        purchase_price = 1000.0
+        expense_ratio = 0.05
+        mutualfund = MutualFund(symbol, name, quantity, purchase_price, expense_ratio)
+
+        self.assertEqual(mutualfund.symbol, symbol)
+        self.assertEqual(mutualfund.name, name)
+        self.assertEqual(mutualfund.quantity, quantity)
+        self.assertEqual(mutualfund.purchase_price, purchase_price)
+        self.assertEqual(mutualfund.type, "mutual_fund")
+        self.assertEqual(mutualfund.expense_ratio, expense_ratio)
+        
+    @patch('requests.get')
+    def test_current_price(self, mock_get):
+        symbol = "ABNB"
+        name = "Airbnb"
+        quantity = 5
+        purchase_price = 110.0
+        investment = Investment(symbol, name, quantity, purchase_price)
+
+        # API response
+        mock_response = mock_get.return_value
+        mock_response.status_code = 200
+        mock_response.text = '''{
+            "Time Series (Daily)": {
+                "2023-05-22": {
+                    "1. open": "1234.12"
+                }
+            }
+        }'''
+
+        price = investment.current_price()
+
+        # Price & mock value should match
+        self.assertEqual(price, 1234.12)
+
+    @patch('requests.get')
+    def test_total_value(self, mock_get):
+        mock_response = mock_get.return_value
+        mock_response.status_code = 200
+        mock_response.text = '''{
+            "Time Series (Daily)": {
+                "2023-05-22": {
+                    "1. open": "1234.12"
+                }
+            }
+        }'''
+
+        # Create portfolio
+        portfolio = Portfolio()
+        stock = Stock("ABNB", "Airbnb", 5, 110.0)
+        bond = Bond("APPL", "Tesla Inc", 10, 100, 0.1)
+
+        # Add investments to portfolio
+        portfolio.add_investment(stock)
+        portfolio.add_investment(bond)
+
+        # Check total value
+        value = stock.current_value() + bond.current_value()
+        self.assertEqual(portfolio.total_value(), value)
+
+
+    # Testing invalid inputs
+    def test_negative_quantity(self):
+        with self.assertRaises(ValueError):
+            Investment("ABNB", "Airbnb", -1, 100.00)
+
+    def test_invalid_data_type(self):
+        with self.assertRaises(ValueError):
+            Investment("ABNB", "Airbnb", True, 100.00)
 
 
 if __name__ == '__main__':
